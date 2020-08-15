5d9923508955f3583dd798d909fbbc3cd9fb8957
michlee1337 <michlee1337@gmail.com>
2020-08-15 02:37:06 +0000

    private funcs that need to be specialized

--- v1/protocol_binding/binder.h
+++ v1/protocol_binding/binder.h
@@ -1,8 +1,6 @@
 #ifndef CLOUDEVENTSCPPSDK_V1_BINDING_BINDER_H
 #define CLOUDEVENTSCPPSDK_V1_BINDING_BINDER_H
 
-#include <memory>
-
 #include <google/protobuf/message.h>
 
 #include "third_party/statusor/statusor.h"
@@ -31,10 +29,63 @@ namespace binding {
 template <typename Message>
 class Binder {
  public:
-
+ 
 // The following operations are protocol-specific and
 // will be overriden for each supported ProtocolBinding
  private:
+  // Const keys used accross ProtocolBindings
+  inline static const std::string kMetadataPrefix = "ce-";
+  inline static const std::string kContenttypePrefix = "application/cloudevents+";
+  inline static const std::string kContenttypeKey = "datacontenttype";
+
+  // _____ Operations used in Unbind Structured _____
+
+  absl::StatusOr<std::string> GetContentType(const Message& message) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  absl::StatusOr<std::string> GetPayload(const Message& message) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  // _____ Operations used in Unbind Binary _____
+
+  absl::Status UnbindMetadata(const Message& message,
+      io::cloudevents::v1::CloudEvent& cloud_event) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  absl::Status UnbindData(const Message& message,
+      io::cloudevents::v1::CloudEvent& cloud_event) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  // _____ Operations used in Bind Structured _____
+
+  absl::Status BindContentType(const std::string& contenttype,
+      Message& message) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  absl::Status BindDataStructured(const std::string& payload,
+      Message& message) {
+    return absl::InternalError("Unimplemented operation");
+  }
+
+  // _____ Operations used in Bind Binary _____
+  absl::Status BindMetadata(const std::string& key,
+      const io::cloudevents::v1::CloudEvent_CloudEventAttribute& val,
+      Message& msg) {
+    return absl::InternalError("UnimplementedOperation");
+  }
+
+  absl::Status BindDataBinary(const std::string& bin_data, Message& msg) {
+    return absl::InternalError("UnimplementedOperation");
+  }
+
+  absl::Status BindDataText(const std::string& text_data, Message& msg) {
+    return absl::InternalError("UnimplementedOperation");
+  }
 };
 
 }  // namespace binding
