dd10efc938b7c04dc92f1c908771dc87098bc8bb
michlee1337 <michlee1337@gmail.com>
2020-08-15 02:50:41 +0000

    pubsub binder

---
+++ v1/protocol_binding/pubsub_binder.cc
@@ -0,0 +1,119 @@
+#include "pubsub_binder.h"
+
+namespace cloudevents {
+namespace binding {
+
+using ::google::pubsub::v1::PubsubMessage;
+using ::io::cloudevents::v1::CloudEvent;
+using ::io::cloudevents::v1::CloudEvent_CloudEventAttribute;
+using ::cloudevents::format::StructuredCloudEvent;
+using ::cloudevents::format::Format;
+using ::cloudevents::format::Formatter;
+using ::cloudevents::formatter_util::FormatterUtil;
+using ::cloudevents::cloudevents_util::CloudEventsUtil;
+
+typedef absl::flat_hash_map<std::string, CloudEvent_CloudEventAttribute>
+  CeAttrMap;
+
+inline static const std::string kPubsubContenttypeKey = "content-type";
+
+// _____ Specializations for Unbind Structured _____
+
+template <>
+absl::StatusOr<std::string> Binder<PubsubMessage>::GetContentType(
+    const PubsubMessage& pubsub_msg) {
+  google::protobuf::Map<std::string, std::string> attrs =
+    pubsub_msg.attributes();
+  auto ind = attrs.find(kPubsubContenttypeKey);
+  if (ind == attrs.end()) {
+    return std::string("");
+  }
+  return ind -> second;
+}
+
+template <>
+absl::StatusOr<std::string> Binder<PubsubMessage>::GetPayload(
+    const PubsubMessage& pubsub_msg) {
+  // get payload and base64 decode
+  return base64::base64_decode(pubsub_msg.data());
+}
+
+// _____ Specializations for Unbind Binary _____
+
+template <>
+absl::Status Binder<PubsubMessage>::UnbindMetadata(
+    const PubsubMessage& pubsub_msg, CloudEvent& cloud_event) {
+  for (auto const& attr : pubsub_msg.attributes()) {
+    std::string key;
+    if (attr.first == kPubsubContenttypeKey) {
+      key = kContenttypeKey;
+    } else if (attr.first.rfind(kMetadataPrefix, 0) == 0){
+      size_t len_prefix = kMetadataPrefix.length();
+      key = attr.first.substr(len_prefix, std::string::npos);
+    }
+    if (auto set_md = CloudEventsUtil::SetMetadata(key, attr.second,
+        cloud_event); !set_md.ok()) {
+      return set_md;
+    }
+  }
+  return absl::OkStatus();
+}
+
+template <>
+absl::Status Binder<PubsubMessage>::UnbindData(
+    const PubsubMessage& pubsub_msg, CloudEvent& cloud_event) {
+  // both CloudEvent.binary_data and Pubsub.payload uses base64 encoding
+  cloud_event.set_binary_data(pubsub_msg.data());
+  return absl::OkStatus();
+}
+
+// _____ Specializations for Bind Structured _____
+
+template <>
+absl::Status Binder<PubsubMessage>::BindContentType(
+    const std::string& contenttype, PubsubMessage& pubsub_msg) {
+  (*pubsub_msg.mutable_attributes())[kPubsubContenttypeKey] = contenttype;
+  return absl::OkStatus();
+}
+
+template <>
+absl::Status Binder<PubsubMessage>::BindDataStructured(
+    const std::string& payload, PubsubMessage& pubsub_msg) {
+  pubsub_msg.set_data(payload);
+  return absl::OkStatus();
+}
+
+// _____ Specializations for Bind Binary _____
+
+template <>
+absl::Status Binder<PubsubMessage>::BindMetadata(const std::string& key,
+    const CloudEvent_CloudEventAttribute& val, PubsubMessage& pubsub_msg) {
+  absl::StatusOr<std::string> val_str = CloudEventsUtil::ToString(val);
+  if (!val_str.ok()) {
+    return val_str.status();
+  }
+  (*pubsub_msg.mutable_attributes())[key] = *val_str;
+  return absl::OkStatus();
+}
+
+template <>
+absl::Status Binder<PubsubMessage>::BindDataBinary(const std::string& bin_data,
+    PubsubMessage& pubsub_msg) {
+  // both CloudEvent.data and pubsub_msg.data are base64 encoded
+  pubsub_msg.set_data(bin_data);
+  return absl::OkStatus();
+}
+
+template <>
+absl::Status Binder<PubsubMessage>::BindDataText(const std::string& text_data,
+    PubsubMessage& pubsub_msg) {
+  absl::StatusOr<std::string> encoded = base64::base64_encode(text_data);
+  if (!encoded.ok()) {
+    return encoded.status();
+  }
+  pubsub_msg.set_data(*encoded);
+  return absl::OkStatus();
+}
+
+}  // namespace binding
+}  // namespace cloudevents
---
+++ v1/protocol_binding/pubsub_binder.h
@@ -0,0 +1,69 @@
+#ifndef CLOUDEVENTSCPPSDK_V1_BINDING_PUBSUBBINDER_H
+#define CLOUDEVENTSCPPSDK_V1_BINDING_PUBSUBBINDER_H
+
+#include "external/googleapis/google/pubsub/v1/pubsub.pb.h"
+#include "v1/protocol_binding/binder.h"
+
+namespace cloudevents {
+namespace binding {
+
+/*
+ * Template specialization to handle PubsubMessages.
+ * For the full template code and comments,
+ * view `//v1/protocol_binding/binder.h`.
+ */
+
+// _____ Specializations for Unbind Structured _____
+
+template <>
+absl::StatusOr<std::string> 
+  Binder<google::pubsub::v1::PubsubMessage>::GetContentType(
+  const google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+template <>
+absl::StatusOr<std::string> 
+  Binder<google::pubsub::v1::PubsubMessage>::GetPayload(
+  const google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+// _____ Specializations for Unbind Binary _____
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::UnbindMetadata(
+  const google::pubsub::v1::PubsubMessage& pubsub_msg, 
+  io::cloudevents::v1::CloudEvent& cloud_event);
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::UnbindData(
+  const google::pubsub::v1::PubsubMessage& pubsub_msg, 
+  io::cloudevents::v1::CloudEvent& cloud_event);
+
+// _____ Specializations for Bind Structured _____
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::BindContentType(
+  const std::string& contenttype, google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::BindDataStructured(
+  const std::string& payload, google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+// _____ Specializations for Bind Binary _____
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::BindMetadata(
+  const std::string& key,
+  const io::cloudevents::v1::CloudEvent_CloudEventAttribute& val,
+  google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::BindDataBinary(
+  const std::string& bin_data, google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+template <>
+absl::Status Binder<google::pubsub::v1::PubsubMessage>::BindDataText(
+  const std::string& text_data, google::pubsub::v1::PubsubMessage& pubsub_msg);
+
+} // binding
+} // cloudevents
+
+#endif // CLOUDEVENTSCPPSDK_V1_BINDING_PUBSUBBINDER_H
